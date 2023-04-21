36879b5cfc1ac5d108d44a12e7176d80cc93f1f5
Katerina <katerinaafadeeva@gmail.com>
2023-04-21 12:22:11 +0300

    navbar and navlinks added

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -3,6 +3,9 @@ import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from '../features/navbar/Navbar';
+import MainPage from '../features/mainPage/MainPage';
+import SignIn from '../features/auth/SignIn';
+import SignUp from '../features/auth/SignUp';
 
 function App(): JSX.Element {
   const dispatch = useDispatch();
@@ -10,7 +13,11 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navbar />}></Route>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<MainPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ frontend/src/features/mainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainPage(): JSX.Element {
+  return <div>MainPage</div>;
+}
+
+export default MainPage;
