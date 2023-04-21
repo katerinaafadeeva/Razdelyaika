03e5894fdfbef6c52787a17ce32a443b788b3103
Matvey <lorrik1703@yandex.ru>
2023-04-21 21:10:05 +0300

    pre releaze

---
+++ frontend/src/features/mainPage/ImgGroup.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ImgGroup(): JSX.Element {
+  return <div>ImgGroup</div>;
+}
+
+export default ImgGroup;
--- frontend/src/features/mainPage/MainPage.tsx
+++ frontend/src/features/mainPage/MainPage.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import './styles/main.css';
 function MainPage(): JSX.Element {
-  return <div>MainPage</div>;
+  return (
+    <div className="full-screen">
+      <div className="full-screen__body">
+        <p className="text__main_p">
+          <span className="tc__y">Разделяй</span> мусор
+        </p>
+        <p> уже сегодня</p>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
