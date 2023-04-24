f720ade947bbbf033579121a7f7e47d51f32e0cc
Maksim Dorofeev <120506295+Maksim22886@users.noreply.github.com>
2023-04-24 16:45:52 +0300

    4 includa

--- backend/api/api.bascet.routes.js
+++ backend/api/api.bascet.routes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
 
-const {}
+const {
+  Product,
+  ProductSize,
+  AddedProduct,
+  Order,
+  Size,
+} = require('../db/models');
+
+router.get('/cart', async (req, res) => {
+  try {
+    const cards = await Product.findAll({
+      raw: true,
+      include: [
+        { model: AddedProduct, include: { model: Order } },
+        { model: ProductSize, include: { model: Size } },
+      ],
+    });
+    res.json(cards);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+module.exports = router;
