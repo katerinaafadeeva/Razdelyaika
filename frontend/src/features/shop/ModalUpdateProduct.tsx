ad195ddb5e57e08c50c6ba0fc765f085c2ce348b
Matvey <lorrik1703@yandex.ru>
2023-04-28 15:14:36 +0300

    go

--- frontend/src/features/shop/ModalUpdateProduct.tsx
+++ frontend/src/features/shop/ModalUpdateProduct.tsx
@@ -18,12 +18,8 @@ const ModalUpdateProduct = ({
 
   const { productId } = useParams();
   const [updproductName, setUpdProductName] = useState(product.productName);
-  const [updproductPrice, setUpdProductPrice] = useState(
-    String(product.productPrice)
-  );
-  const [updproductDescript, setUpdproductDescript] = useState(
-    product.productDescript
-  );
+  const [updproductPrice, setUpdProductPrice] = useState(String(product.productPrice));
+  const [updproductDescript, setUpdproductDescript] = useState(product.productDescript);
   const { sizes } = useSelector((store: RootState) => store.productsState);
 
   const handleUpdateProduct = (e: React.FormEvent<HTMLFormElement>): void => {
@@ -51,24 +47,20 @@ const ModalUpdateProduct = ({
                 onClick={showModalUpdate}
                 style={{
                   fontSize: '1.8rem',
-                }}
-              >
+                }}>
                 <span aria-hidden="true">&times;</span>
               </button>
               <div className="w-full px-4 ">
                 <div className="overflow-hidden rounded-lg bg-white">
-                  <div className="-mx-4 flex flex-wrap">
-                    <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+                  <div className="" style={{ padding: '20px' }}>
+                    <div className="">
                       <form
                         className="mb-12"
                         encType="multipart/form-data"
                         onSubmit={handleUpdateProduct}
                         // onChange={formChange}
                       >
-                        <label
-                          htmlFor=""
-                          className="mb-3 block text-base font-medium text-black"
-                        >
+                        <label htmlFor="" className="mb-3 block text-base font-medium text-black">
                           Наименование продукта
                         </label>
                         <input
@@ -80,10 +72,7 @@ const ModalUpdateProduct = ({
                           onChange={(e) => setUpdProductName(e.target.value)}
                           name="name"
                         />
-                        <label
-                          htmlFor=""
-                          className="mb-3 block text-base font-medium text-black"
-                        >
+                        <label htmlFor="" className="mb-3 block text-base font-medium text-black">
                           Цена
                         </label>
                         <input
@@ -95,25 +84,17 @@ const ModalUpdateProduct = ({
                           onChange={(e) => setUpdProductPrice(e.target.value)}
                           name="price"
                         />
-                        <label
-                          htmlFor=""
-                          className="mb-3 block text-base font-medium text-black"
-                        >
+                        <label htmlFor="" className="mb-3 block text-base font-medium text-black">
                           Описание
                         </label>
                         <textarea
                           placeholder="Описание"
                           className="border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
                           value={updproductDescript}
-                          onChange={(e) =>
-                            setUpdproductDescript(e.target.value)
-                          }
+                          onChange={(e) => setUpdproductDescript(e.target.value)}
                           name="description"
                         />
-                        <label
-                          htmlFor=""
-                          className="mb-3 block text-base font-medium text-black"
-                        >
+                        <label htmlFor="" className="mb-3 block text-base font-medium text-black">
                           Доступные размеры
                         </label>
                         {sizes.map((size) => (
@@ -140,8 +121,7 @@ const ModalUpdateProduct = ({
                         <ProductUploader />
                         <button
                           type="submit"
-                          className="bg-primary inline-flex items-center justify-center rounded-full py-4 px-5 text-center text-base font-normal text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
-                        >
+                          className="change_btn_admin bg-primary inline-flex items-center justify-center rounded-full py-4 px-5 text-center text-base font-normal text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
                           Изменить
                         </button>
                       </form>
