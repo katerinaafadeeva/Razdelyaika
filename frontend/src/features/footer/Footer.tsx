b06a04cae6d558e992841d1525f45824c6bf926f
Matvey <lorrik1703@yandex.ru>
2023-04-28 12:34:00 +0300

    main Page

--- frontend/src/features/footer/Footer.tsx
+++ frontend/src/features/footer/Footer.tsx
@@ -7,7 +7,7 @@ import instLogo from './icons/Inst.svg';
 
 function Footer(): JSX.Element {
   return (
-    <footer className="relative z-10 bg-white pt-20 pb-10 lg:pt-[120px] lg:pb-20 footer">
+    <footer className="relative z-10 bg-white  footer">
       <div className="container mx-auto">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 sm:w-2/3 lg:w-3/12">
